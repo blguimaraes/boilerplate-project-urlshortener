37a0c6115aca9a715c54b73937d4da9c4a4e20ab
Bruno Leão Guimarães <69772262+blguimaraes@users.noreply.github.com>
2023-01-26 18:55:05 -0300

    Sollution v1

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const { nextTick } = require('process');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +20,51 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// Parsing post content
+app.use('/api/shorturl/', express.json());
+app.use('/api/shorturl/', express.urlencoded({extended: true}));
+
+var shortenedUrl = {};
+
+// My url shortener API endpoint
+app.post('/api/shorturl/', function(req, res){  
+  var postUrl = req.body.url;
+  var checkHostName;
+  
+  if ( postUrl.substring(0, 8) == 'https://'){
+    checkHostName = postUrl.slice(8);
+  } else if (postUrl.substring(0, 7) == 'http://') {
+    checkHostName = postUrl.slice(7);
+  } else {
+    res.json({ error: 'invalid url' });
+  };
+
+  dns.lookup(checkHostName.substring(0, checkHostName.indexOf('/')), function(err){
+    if(err && err.code == 'ENOTFOUND'){
+      res.json({ error: 'invalid url' });
+    }
+    else {
+      shortenedUrl = {
+        original_url : postUrl,
+        short_url : Math.floor(Math.random() * 1000)
+        };
+      res.json(shortenedUrl);
+      return shortenedUrl;
+    };
+  });
+});
+
+app.get('/api/shorturl/:short_url', function(req, res, next){
+  async () => {
+    if( req.params.short_url != shortenedUrl.short_url ){
+    res.redirect('/');
+  }};
+  next();
+  }, function(req, res) {
+    res.redirect(301, shortenedUrl.original_url);
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
